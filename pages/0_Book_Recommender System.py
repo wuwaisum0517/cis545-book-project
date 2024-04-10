e9f70dcff2148596f105fab2a383ae85df7509f9
wuwaisum0517 <85812897+wuwaisum0517@users.noreply.github.com>
2024-04-10 15:40:33 +0800

    Graph update

--- pages/0_Book_Recommender System.py
+++ pages/0_Book_Recommender System.py
@@ -2,33 +2,50 @@
 This is the Book Recommender System Page.
 2024-04-09
 Working 0%
+Testing connection using csv file
+SQL Check
 """
 from urllib.error import URLError
 
 import altair as alt
 import pandas as pd
 
+
 import streamlit as st
 from streamlit.hello.utils import show_code
+
+import numpy as np
+import datetime as dt
+import re
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import cm
 
 
 def book_recommendation():
     def get_data():
         AWS_BUCKET_URL = "https://cis5450-project-test.s3.amazonaws.com/"
-        df_books = pd.read_csv(AWS_BUCKET_URL + "Books.csv")
-        return df_books.set_index("Publisher")
+        df_books = pd.read_csv(AWS_BUCKET_URL + "cleaned_data_small.csv",encoding="utf-8")
+        return df_books
 
     try:
         df = get_data()
-        publisher_input = st.multiselect(
-            "Choose a Publisher", list(df.index), ["Dell", "Oxford University Press"]
+        isbn_input = st.multiselect(
+            "Choose a Publisher", list(df.index), [1, 2]
         )
 
-        if not publisher_input:
-            st.error("Please select at least one Publisher.")
+        if not isbn_input:
+            st.error("Please select at least one ISBN.")
         else:
-            data = df.loc[publisher_input]
-            data = pd.melt(data, id_vars=['ISBN', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher'])
+            data = df [['ISBN','Book-Title','Book-Author']]
+            fig, ax = plt.subplots(figsize = (8,8))
+            fig.patch.set_visible(False)
+            ax.axis('off')
+            ax.axis('tight')
+            ax.table(cellText=data.values,colLabels=data.columns,loc = 'center')
+            fig.tight_layout()
+            st.pyplot(plt.gcf()) # instead of plt.show()
 
 
     except URLError as e:
@@ -45,9 +62,9 @@ def book_recommendation():
 
 st.set_page_config(page_title="Book Recommender System Demo", page_icon="📊")
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write()
+st.markdown("# Book Recommender System")
+st.sidebar.header("Book Recommender System")
+st.write("")
 
 book_recommendation()
 show_code(book_recommendation)
