960bfc83a49accc7ebb951f1aec46f012de44c8a
wuwaisum0517 <85812897+wuwaisum0517@users.noreply.github.com>
2024-04-18 23:25:27 +0800

    2024-04-17 SQLRequirement Test

--- pages/0_Book_Recommender System.py
+++ pages/0_Book_Recommender System.py
@@ -19,7 +19,7 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import cm
-import mysqlclient.connector
+import mysql.connector
 from sklearn.metrics.pairwise import cosine_similarity
 
 def book_recommendation():
@@ -40,7 +40,7 @@ def book_recommendation():
         Database connection
         secret variable saved in steamlit
         """
-        mydb = mysqlclient.connector.connect(
+        mydb = mysql.connector.connect(
             host=st.secrets["db_host"],
             user=st.secrets["db_user"],
             password=st.secrets["db_password"]
